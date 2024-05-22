499ba5bbed3bc7677e3ed03c677e52c3f4607c8e
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-22 20:11:19 -0300

    setup: first commit, setup done

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ...pluginReactConfig,
+        rules: { ...pluginReactConfig.rules, 'react/react-in-jsx-scope': 0 }
+    },
+    {
+        ignores: ['build/*', 'node_modules/*']
+    },
+    eslintConfigPrettier
+];
