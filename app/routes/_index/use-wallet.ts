a839aa1600b7020f76a5e71604b316ff2cef76b1
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-24 21:13:54 -0300

    enhanc(auth form): login submit btn overclicking avoided

--- app/routes/_index/use-wallet.ts
+++ app/routes/_index/use-wallet.ts
@@ -1,24 +1,32 @@
 import { useState } from 'react';
 
-type WalletData = Record<'accounts' | 'error', any>;
+type WalletData = Record<'accounts' | 'error', any> & {
+    status: 'idle' | 'pending' | 'success' | 'error';
+};
 type WalletDispatch = {
     getAccount: () => Promise<WalletData['accounts']>;
 };
 
 export function useWallet(): [WalletData, WalletDispatch] {
     const [data, setData] = useState<WalletData>({
+        status: 'idle',
         accounts: null,
         error: null
     });
 
     const getAccount = () => {
+        setData(prev => ({ ...prev, status: 'pending' }));
         const provider = window.ethereum;
 
         if (provider.isConnected()) {
             return provider
                 .request({ method: 'eth_requestAccounts' })
                 .then((accounts: any) => {
-                    setData(prev => ({ ...prev, accounts: accounts }));
+                    setData(prev => ({
+                        ...prev,
+                        accounts: accounts,
+                        status: 'success'
+                    }));
                     return accounts;
                 })
                 .catch((err: any) => {
@@ -26,7 +34,8 @@ export function useWallet(): [WalletData, WalletDispatch] {
                         err.code === 4001 ? 'Please connect to MetaMask.' : err;
                     setData(prev => ({
                         ...prev,
-                        error: errFallback
+                        error: errFallback,
+                        status: 'error'
                     }));
                     throw errFallback;
                 });
