649507c2e925fd2bb44527e0907df837e2403cd4
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-26 16:43:20 -0300

    enhanc: use wallet and fetcher colocated to page action and loader level

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -3,6 +3,7 @@ import { json } from '@remix-run/node';
 import { useFetcher, useLoaderData } from '@remix-run/react';
 import { LoginForm, LoginFormKey } from './login-form';
 import * as walletUseCase from '~/use-cases/wallet';
+import { useWallet } from '~/use-cases/wallet/use-wallet';
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const wallet = await walletUseCase.getWallet(request);
@@ -14,18 +15,44 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function Home() {
     const fetcher = useFetcher();
-    const { wallet } = useLoaderData<typeof loader>();
-    let address = wallet.address;
+    const loaderData = useLoaderData<typeof loader>();
+    const [wallet, dispatch] = useWallet();
+
+    let address = loaderData.wallet.address;
 
     if (fetcher.formData?.has(LoginFormKey.address)) {
         address = fetcher.formData.get(LoginFormKey.address) as string;
     }
 
+    const onSubmitLogin = () => {
+        dispatch
+            .connectWallet({
+                onAccountsChange: async (accounts: string[]) => {
+                    const formData = new FormData();
+                    formData.append(LoginFormKey.address, accounts[0]);
+                    fetcher.submit(formData, { method: 'POST' });
+                }
+            })
+            .then(accounts => {
+                const formData = new FormData();
+                formData.append(LoginFormKey.address, accounts[0]);
+                fetcher.submit(formData, { method: 'POST' });
+            });
+    };
+
     return (
         <>
             <h1>Hello world! Private key:{address ?? 'not provided'}</h1>
-            <h1>Current balance: {wallet.ethBalance ?? 'not informed'}</h1>
-            <LoginForm />
+            <h1>
+                Current balance:{' '}
+                {loaderData.wallet.ethBalance ?? 'not informed'}
+            </h1>
+            <LoginForm
+                fetcher={fetcher}
+                onSubmit={onSubmitLogin}
+                isPending={wallet.status === 'pending'}
+                error={wallet.error}
+            />
         </>
     );
 }
