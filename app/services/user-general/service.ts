ea291cdca6620f73b6011b706e53880493faa0f3
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-26 10:27:33 -0300

    service: wallet service renamed

--- app/services/user-general/service.ts
+++
@@ -1,8 +0,0 @@
-import { requestBuilder } from '~/utils/request-builder';
-import { endpoint } from './endpoint';
-import type { GetBalanceResponse } from './types';
-
-export const getBalance = (userAccountId: string) =>
-    requestBuilder
-        .url(endpoint.getBalance.getUrl(userAccountId))
-        .inspect<GetBalanceResponse>();
--- app/services/wallet/endpoint.ts
+++ app/services/wallet/endpoint.ts
@@ -2,6 +2,6 @@ const inspectBaseUrl = 'http://localhost:8080/inspect';
 
 export const endpoint = {
     getBalance: {
-        getUrl: (id: string) => `${inspectBaseUrl}/wallet/ether/${id}`
+        getUrl: (address: string) => `${inspectBaseUrl}/wallet/ether/${address}`
     }
 };
