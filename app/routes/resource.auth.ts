12709d0c09a4ef051162eba2e2b4d005cb41ea5e
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-29 02:14:29 -0300

    chore: auth loader relocated

--- app/routes/resource.auth.ts
+++ app/routes/resource.auth.ts
@@ -1,11 +1,31 @@
-import { type ActionFunctionArgs, json } from '@remix-run/node';
+import {
+    type ActionFunctionArgs,
+    type LoaderFunctionArgs,
+    json
+} from '@remix-run/node';
 import * as walletUseCase from '~/use-cases/wallet/functions';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+    const wallet = await walletUseCase.getWallet(request);
+
+    return json(wallet);
+}
 
 export async function action({ request }: ActionFunctionArgs) {
     const { cookie, serializedCookie } =
         await walletUseCase.handleSubmitWalletAddress(request);
+    let wallet: walletUseCase.WalletData | null = null;
 
-    return json(cookie, {
-        headers: { 'Set-Cookie': serializedCookie }
-    });
+    if (cookie.address) {
+        wallet = await walletUseCase.getWallet(undefined, cookie.address);
+    }
+
+    const payloadFallback = wallet ?? cookie;
+
+    return json(
+        { ...payloadFallback, submitted: 'ok' },
+        {
+            headers: { 'Set-Cookie': serializedCookie }
+        }
+    );
 }
