6decf2003ccf847708b3891b6c791ffa82792168
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-29 20:02:05 -0300

    enhanc: dao member feedback added to games route

--- app/routes/games/route.tsx
+++ app/routes/games/route.tsx
@@ -1,8 +1,17 @@
+import { useFetcher } from '@remix-run/react';
+import { AuthFetcherKey } from '~/components/header';
+import type { WalletData } from '~/use-cases/wallet';
+
 export default function Games() {
+    const wallet = useFetcher<WalletData>({ key: AuthFetcherKey });
+
     return (
         <>
             <h1>Games page</h1>
             <p className="font-body">Simple body</p>
+            {wallet.data?.isDAOMember && (
+                <p>{"You're a DAO member! Welcome!!!"}</p>
+            )}
         </>
     );
 }
