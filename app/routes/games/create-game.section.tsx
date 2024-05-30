ebbb9a0b304fe0d7715dac3ebd0262608246ae76
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-29 22:28:55 -0300

    feat: create game modal impl

--- app/routes/games/create-game.section.tsx
+++ app/routes/games/create-game.section.tsx
@@ -1,6 +1,25 @@
-import { Button, Card, CardFooter, CardHeader, Image } from '@nextui-org/react';
+import {
+    Button,
+    Card,
+    CardFooter,
+    CardHeader,
+    DateInput,
+    DateValue,
+    Divider,
+    Input,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalProps,
+    useDisclosure
+} from '@nextui-org/react';
+import { useState } from 'react';
 
 export const CreateGameSection = () => {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
     return (
         <section className="py-6">
             <Card className="w-full h-[128px] col-span-12 bg-secondary-400 sm:col-span-5">
@@ -14,7 +33,7 @@ export const CreateGameSection = () => {
                 </CardHeader>
                 <CardFooter className="absolute bottom-0 z-10 justify-between">
                     <p className="text-gray-200">
-                        Create new game to general users betting and spark the
+                        Create new game for general users betting and spark the
                         hype! 💥
                     </p>
                     <Button
@@ -22,11 +41,106 @@ export const CreateGameSection = () => {
                         color="secondary"
                         radius="full"
                         size="sm"
+                        onClick={onOpen}
                     >
                         Create game
                     </Button>
                 </CardFooter>
             </Card>
+            <CreateGameModal isOpen={isOpen} onOpenChange={onOpenChange} />
         </section>
+    );
+};
+const CreateGameModal = (
+    props: Pick<ModalProps, 'isOpen' | 'onOpenChange'>
+) => {
+    const [bettingDate, setBettingDate] = useState<
+        Record<'startTime' | 'endTime', DateValue | null>
+    >({
+        startTime: null,
+        endTime: null
+    });
+
+    const onStartTimeSelected = (selectedDate: DateValue) =>
+        setBettingDate(prev => ({ ...prev, startTime: selectedDate }));
+    const onEndTimeSelected = (selectedDate: DateValue) =>
+        setBettingDate(prev => ({ ...prev, endTime: selectedDate }));
+
+    return (
+        <Modal
+            isOpen={props.isOpen}
+            onOpenChange={props.onOpenChange}
+            placement="top-center"
+            backdrop="blur"
+        >
+            <ModalContent>
+                {onClose => (
+                    <>
+                        <ModalHeader className="flex flex-col gap-1">
+                            Create game
+                        </ModalHeader>
+                        <ModalBody>
+                            <h3>Validation function</h3>
+                            <Input
+                                autoFocus
+                                label="Name"
+                                placeholder="Enter function name"
+                                variant="bordered"
+                            />
+                            <Input
+                                label="Code"
+                                placeholder="Enter function code"
+                                variant="bordered"
+                            />
+                            <Divider />
+                            <h3>Game infos</h3>
+                            <Input
+                                label="Home pick"
+                                placeholder="Enter the first betting pick option"
+                                variant="bordered"
+                            />
+                            <Input
+                                label="Away pick"
+                                placeholder="Enter the second betting pick option"
+                                variant="bordered"
+                            />
+                            <Input
+                                label="Token"
+                                placeholder="Enter the token used to betting"
+                                variant="bordered"
+                            />
+                            <DateInput
+                                label="Starts at"
+                                variant="bordered"
+                                granularity="minute"
+                                value={bettingDate.startTime}
+                                onChange={onStartTimeSelected}
+                                // placeholder="Select the date/time to start betting"
+                            />
+                            <DateInput
+                                label="Ends at"
+                                variant="bordered"
+                                granularity="minute"
+                                value={bettingDate.endTime}
+                                onChange={onEndTimeSelected}
+                                // placeholder="Select the date/time to finish betting"
+                            />
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button
+                                color="default"
+                                variant="flat"
+                                onPress={onClose}
+                            >
+                                Cancel
+                            </Button>
+                            <Button color="secondary" onPress={onClose}>
+                                Create
+                            </Button>
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
     );
 };
