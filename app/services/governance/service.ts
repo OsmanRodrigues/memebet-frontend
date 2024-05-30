8425cc245af819d1fb291aa1c9fb866ce40504f9
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-30 16:25:27 -0300

    chore(service): create validation function and create game services impl

--- app/services/governance/service.ts
+++ app/services/governance/service.ts
@@ -1,5 +1,42 @@
 import { requestBuilder } from '~/utils/request-builder';
 import { endpoint } from './endpoint';
+import { advanceABI } from '../shared-constants';
+import { transformUTF8toHex } from '~/utils/transformer';
+import type { CreateGameArgs, CreateValidationFunctionArgs } from './type';
 
 export const getDAOMembersList = () =>
     requestBuilder.url(endpoint.listDAOMembers).inspect<string[]>();
+export const createValidationFunction = ({
+    functionCode,
+    functionName,
+    provider,
+    signerAddress
+}: CreateValidationFunctionArgs) => {
+    return requestBuilder
+        .config({ provider, signerAddress })
+        .send<undefined>(advanceABI.addValidationFunction.name, [
+            transformUTF8toHex(functionName),
+            transformUTF8toHex(functionCode)
+        ]);
+};
+export const createGame = ({
+    home,
+    away,
+    token,
+    start,
+    end,
+    validatorFunctionName,
+    provider,
+    signerAddress
+}: CreateGameArgs) => {
+    return requestBuilder
+        .config({ provider, signerAddress })
+        .send<undefined>(advanceABI.createGame.name, [
+            transformUTF8toHex(home),
+            transformUTF8toHex(away),
+            token,
+            start,
+            end,
+            transformUTF8toHex(validatorFunctionName)
+        ]);
+};
