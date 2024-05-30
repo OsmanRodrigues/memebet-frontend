d463c6967779ef13fa61d374930443697f7d0061
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-30 16:19:44 -0300

    chore: transformer utils adjusted to use web3 utils package

--- app/utils/transformer.ts
+++ app/utils/transformer.ts
@@ -1,7 +1,7 @@
-export const transformHexToUTF8 = (str: string) =>
-    decodeURIComponent(
-        str
-            .replace('0x', '')
-            .replace(/\s+/g, '')
-            .replace(/[0-9a-f]{2}/g, '%$&')
-    );
+import { utils } from 'web3';
+
+export const transformHexToUTF8 = (str: string): string => utils.toUtf8(str);
+export const transformUTF8toHex = (string: string): string => {
+    const hex = utils.fromUtf8(string);
+    return utils.padRight(hex, 66 - '0x'.length);
+};
