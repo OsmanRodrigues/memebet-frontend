8bfe228a1f9f8942c0633fe71574314cf7da46c5
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-30 16:25:27 -0300

    chore(service): create validation function and create game services impl

--- app/services/shared-constants.ts
+++ app/services/shared-constants.ts
@@ -1,1 +1,15 @@
 export const inspectBaseUrl = 'http://localhost:8080/inspect';
+export const dappAddress = '0xab7528bb862fB57E8A2BCd567a2e929a0Be56a5e';
+export const advanceABI = {
+    addValidationFunction: {
+        name: 'addValidationFunction',
+        abi: 'function addValidationFunction(bytes32 name, bytes32 functionString)'
+    },
+    createGame: {
+        name: 'createGame',
+        abi: 'function createGame(bytes32 home, bytes32 away, address token , uint256 start, uint256 end, bytes32 validatorFunctionName)'
+    }
+};
+export const advanceABIMap = Object.values(advanceABI).map(
+    currentABI => currentABI.abi
+);
