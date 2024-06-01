ab1fe70345502290e123417a1411db42074fcbae
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-31 23:35:14 -0300

    refactor(resource): governance use case client and server consume adjusted

--- app/routes/resource.game.ts
+++ app/routes/resource.game.ts
@@ -1,11 +1,12 @@
 import { json } from '@remix-run/node';
-import * as governanceUseCase from '~/use-cases/governance/functions';
+import * as governanceUseCase from '~/use-cases/governance/functions.server';
+import * as governanceUseCaseClient from '~/use-cases/governance/functions.client';
 import type { ActionFunctionArgs } from '@remix-run/node';
 import type { ClientActionFunctionArgs } from '@remix-run/react';
+import type { PreActionResponse } from '~/use-cases/governance/type';
 
 export async function action({ request }: ActionFunctionArgs) {
     const preActionRes = await governanceUseCase.preAction(request);
-
     if (preActionRes.error)
         throw new Response(preActionRes.error, {
             status: preActionRes.status
@@ -15,21 +16,22 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export async function clientAction({ serverAction }: ClientActionFunctionArgs) {
-    const serverRes =
-        await serverAction<Required<governanceUseCase.PreActionResponse>>();
+    const serverRes = await serverAction<Required<PreActionResponse>>();
     const { formData, wallet } = serverRes;
 
     const { functionName, functionCode, ...leftovers } = formData;
 
     if (functionName && functionCode) {
-        return await governanceUseCase.handleSubmitCreateValidationFunction({
-            provider: window.ethereum,
-            signerAddress: wallet.address!,
-            functionName,
-            functionCode
-        });
+        return await governanceUseCaseClient.handleSubmitCreateValidationFunction(
+            {
+                provider: window.ethereum,
+                signerAddress: wallet.address!,
+                functionName,
+                functionCode
+            }
+        );
     } else {
-        return await governanceUseCase.handleSubmitCreateGameFunction({
+        return await governanceUseCaseClient.handleSubmitCreateGame({
             provider: window.ethereum,
             signerAddress: wallet.address!,
             ...leftovers
