40c8f7fbbf30f52e994e0b725071e766390de2c2
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-01 01:26:20 -0300

    chore(route): get games list use case consume added

--- app/routes/games/route.tsx
+++ app/routes/games/route.tsx
@@ -1,16 +1,41 @@
-import { useFetcher } from '@remix-run/react';
+import {
+    isRouteErrorResponse,
+    useFetcher,
+    useRouteError
+} from '@remix-run/react';
+import * as gamesUseCase from '~/use-cases/games/functions';
 import { AuthFetcherKey } from '~/components/header';
-import type { WalletData } from '~/use-cases/wallet';
 import { CreateGameSection } from './create-game.section';
+import { GamesListSection } from './games-list.section';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import type { WalletData } from '~/use-cases/wallet';
+import { ErrorFallback } from '~/components/error-fallback';
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+    console.log('request.method, params ->', request.method, params);
+    return await gamesUseCase.getGamesList();
+}
 
 export default function Games() {
     const wallet = useFetcher<WalletData>({ key: AuthFetcherKey });
 
     return (
         <>
-            {wallet.data?.isDAOMember && <CreateGameSection />}
             <h1>Games page</h1>
-            <p className="font-body">Simple body</p>
+            {wallet.data?.isDAOMember && <CreateGameSection />}
+            <GamesListSection />
         </>
+    );
+}
+
+export function ErrorBoundary() {
+    const error = useRouteError() as any;
+
+    return (
+        <ErrorFallback
+            title="Games List Page Error"
+            error={error}
+            isRouteError={isRouteErrorResponse(error)}
+        />
     );
 }
