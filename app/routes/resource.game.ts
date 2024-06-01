14a9c484b55cb1643b5d7074091039fece1af45f
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-05-31 21:43:05 -0300

    chore(resource): game resource governance use case consume added

--- app/routes/resource.game.ts
+++ app/routes/resource.game.ts
@@ -1,80 +1,38 @@
 import { json } from '@remix-run/node';
-import { getWalletCookie } from '~/use-cases/wallet/cookies';
-import {
-    createGame,
-    createValidationFunction
-} from '~/services/governance/service';
+import * as governanceUseCase from '~/use-cases/governance/functions';
 import type { ActionFunctionArgs } from '@remix-run/node';
 import type { ClientActionFunctionArgs } from '@remix-run/react';
-import type {
-    CreateFunctionFetcherData,
-    CreateGameFetcherData
-} from './games/create-game.section';
 
 export async function action({ request }: ActionFunctionArgs) {
-    const wallet = await getWalletCookie(request);
+    const preActionRes = await governanceUseCase.preAction(request);
 
-    if (wallet.address) {
-        const formData = Object.fromEntries(await request.formData());
-        return json({ wallet, formData });
-    }
+    if (preActionRes.error)
+        throw new Response(preActionRes.error, {
+            status: preActionRes.status
+        });
 
-    throw new Response('User not authenticated!', {
-        status: 401
-    });
+    return json(preActionRes);
 }
 
 export async function clientAction({ serverAction }: ClientActionFunctionArgs) {
-    const serverRes = await serverAction<{
-        wallet: { address: string };
-        formData: CreateFunctionFetcherData & CreateGameFetcherData;
-    }>();
+    const serverRes =
+        await serverAction<Required<governanceUseCase.PreActionResponse>>();
     const { formData, wallet } = serverRes;
-    const validateFormData = (formData: Partial<typeof serverRes.formData>) => {
-        if (Object.values(formData).some(value => !value))
-            return {
-                error: 'Create form data must be filled with all required fields.'
-            };
-    };
+
     const { functionName, functionCode, ...leftovers } = formData;
 
     if (functionName && functionCode) {
-        const createValidationFunctionRes = await createValidationFunction({
+        return await governanceUseCase.handleSubmitCreateValidationFunction({
             provider: window.ethereum,
-            signerAddress: wallet.address,
-            functionName: formData.functionName,
-            functionCode: formData.functionCode
+            signerAddress: wallet.address!,
+            functionName,
+            functionCode
         });
-
-        if (!createValidationFunctionRes.ok)
-            return {
-                error:
-                    createValidationFunctionRes.error ??
-                    'An error occurried while create the function.'
-            };
-
-        return createValidationFunctionRes;
     } else {
-        validateFormData(leftovers);
-
-        const createGameRes = await createGame({
+        return await governanceUseCase.handleSubmitCreateGameFunction({
             provider: window.ethereum,
-            signerAddress: wallet.address,
-            home: formData.home,
-            away: formData.away,
-            token: formData.token,
-            start: new Date(formData.start).getTime(),
-            end: new Date(formData.end).getTime(),
-            validatorFunctionName: formData.validatorFunctionName
+            signerAddress: wallet.address!,
+            ...leftovers
         });
-
-        if (!createGameRes.ok)
-            return {
-                error:
-                    createGameRes.error ??
-                    'An error occurried while create the game.'
-            };
-
-        return createGameRes;
     }
 }
