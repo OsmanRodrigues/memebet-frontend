17ab555f9e44ef4419f234689c4efc84486c564a
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-02 01:44:30 -0300

    chore(utils): text shortner and get date str utils impl

--- app/utils/transformer.ts
+++ app/utils/transformer.ts
@@ -5,3 +5,9 @@ export const transformUTF8toHex = (string: string): string => {
     const hex = utils.fromUtf8(string);
     return utils.padRight(hex, 66 - '0x'.length);
 };
+export const textShortener = (text: string, maxSize = 8): string => {
+    const offsetSize = 3;
+    if (text.length <= maxSize || text.length < offsetSize) return text;
+
+    return `${text.slice(0, maxSize - offsetSize)}...${text.slice(-offsetSize)}`;
+};
