b19efeca26b68ba3f3df8aa7d085b2765e17512b
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-01 22:40:07 -0300

    chore(util): log utils timestamp added

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -1,9 +1,19 @@
 const baseCSS =
     'color: #000000; font-style: italic; background-color: $color;padding: 2px';
+const applyTimestamp = (msg: string) => `[${new Date().toISOString()}]` + msg;
 
 export const logError = (msg: string) =>
-    console.error('%c'.concat(msg), baseCSS.replace('$color', '#f35b69'));
+    console.error(
+        '%c'.concat(applyTimestamp(msg)),
+        baseCSS.replace('$color', '#f35b69')
+    );
 export const logInfo = (msg: string) =>
-    console.info('%c'.concat(msg), baseCSS.replace('$color', '#4d76ee'));
+    console.info(
+        '%c'.concat(applyTimestamp(msg)),
+        baseCSS.replace('$color', '#4d76ee')
+    );
 export const logSuccess = (msg: string) =>
-    console.log('%c'.concat(msg), baseCSS.replace('$color', '#43c358'));
+    console.log(
+        '%c'.concat(applyTimestamp(msg)),
+        baseCSS.replace('$color', '#43c358')
+    );
