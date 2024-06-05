d0146dbf18be6a7eeb9b50c46b892104cffac141
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-05 01:10:10 -0300

    service: get game by id enabled

--- app/services/games/enpoint.ts
+++ app/services/games/enpoint.ts
@@ -1,5 +1,8 @@
 import { inspectBaseUrl } from '../shared-constants';
 
 export const endpoint = {
+    getGameById: {
+        getUrl: (id: string) => `${inspectBaseUrl}/games/getGame/${id}`
+    },
     getGamesList: `${inspectBaseUrl}/games/currentGames`
 };
