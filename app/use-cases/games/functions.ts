b2ead1cdd3adb6889bb17130b7cd900c2888c75b
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-05 01:13:42 -0300

    use case: get game by id function impl

--- app/use-cases/games/functions.ts
+++ app/use-cases/games/functions.ts
@@ -1,17 +1,49 @@
 import * as gamesService from '~/services/games/service';
 import type { Game } from '~/services/games/type';
 
-export type GameViewModel = Pick<
+export type GameListViewModel = Pick<
     Game,
     'id' | 'picks' | 'startTime' | 'endTime' | 'betPool'
 >;
-export type GetGamesListResponse = {
-    gamesList?: GameViewModel[];
+export type GameViewModel = Pick<
+    Game,
+    | 'id'
+    | 'picks'
+    | 'startTime'
+    | 'endTime'
+    | 'fees'
+    | 'playerIds'
+    | 'currentOdds'
+>;
+type GamesUseCaseBaseResponse = {
     message?: string;
     error?: any;
     status?: number;
 };
+export type GetGameByIdResponse = GamesUseCaseBaseResponse & {
+    game?: GameListViewModel;
+};
+export type GetGamesListResponse = GamesUseCaseBaseResponse & {
+    gamesList?: GameViewModel[];
+};
 
+export const getGameById = async (
+    gameId: string
+): Promise<GetGameByIdResponse> => {
+    const gamesServiceRes = await gamesService.getGameById(gameId);
+
+    if (!gamesServiceRes.ok) {
+        return {
+            message: `Oops! An error occurred on get game "${gameId}".`,
+            error: gamesServiceRes.error,
+            status: 404
+        };
+    } else if (typeof gamesServiceRes.data === 'string')
+        return { message: gamesServiceRes.data };
+    else if (gamesServiceRes.data) return { game: gamesServiceRes.data };
+
+    return {};
+};
 export const getGamesList = async (): Promise<GetGamesListResponse> => {
     const gamesServiceRes = await gamesService.getGamesList();
 
