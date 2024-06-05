94b8eb90b9aeb2a5029451c0028897258c250c77
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-05 01:10:10 -0300

    service: get game by id enabled

--- app/services/games/service.ts
+++ app/services/games/service.ts
@@ -2,5 +2,7 @@ import { requestBuilder } from '~/utils/request-builder';
 import { endpoint } from './enpoint';
 import type { Game } from './type';
 
+export const getGameById = (id: string) =>
+    requestBuilder.url(endpoint.getGameById.getUrl(id)).inspect<Game>();
 export const getGamesList = () =>
     requestBuilder.url(endpoint.getGamesList).inspect<Game[]>();
