0d5939714ef333b4c69175d55e8ce7b7ed63c7b0
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-06 16:59:16 -0300

    enhanc: loggin and funds check before place a bet impl

--- app/use-cases/games/functions.client.ts
+++ app/use-cases/games/functions.client.ts
@@ -2,17 +2,24 @@ import * as gamesService from '~/services/games/service';
 import { validateFormData } from '../governance/helpers';
 
 import type { PlaceABetArgs } from '~/services/games/type';
+import type { PlaceABetResponse } from './type';
 
-export const submitPlaceABet = async (args: PlaceABetArgs) => {
+export const submitPlaceABet = async (
+    args: PlaceABetArgs
+): Promise<PlaceABetResponse> => {
     validateFormData(args);
 
     const placeABetRes = await gamesService.placeABet(args);
 
     if (!placeABetRes.ok)
         return {
+            ...placeABetRes,
             error:
                 placeABetRes.error ?? 'An error occurried while place the bet.'
         };
 
-    return placeABetRes;
+    return {
+        ok: placeABetRes.ok,
+        transactionHash: placeABetRes.data?.transactionHash
+    };
 };
