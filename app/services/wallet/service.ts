87979273dab992d8cbf220762af24c3980a8849a
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-08 02:02:08 -0300

    chore:  deposit token service enabled

--- app/services/wallet/service.ts
+++ app/services/wallet/service.ts
@@ -1,7 +1,14 @@
 import { requestBuilder } from '~/utils/request-builder';
 import { endpoint } from './endpoint';
-import type { GetEtherBalanceResponse } from './types';
 
+import type { DepositTokenArgs, GetEtherBalanceResponse } from './types';
+
+export const depositToken = ({
+    amount,
+    provider,
+    signerAddress
+}: DepositTokenArgs) =>
+    requestBuilder.config({ provider, signerAddress }).depositToken(amount);
 export const getEthBalance = (walletAddress: string) =>
     requestBuilder
         .url(endpoint.getBalance.getUrl(walletAddress))
