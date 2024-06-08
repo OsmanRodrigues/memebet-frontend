05740b2ab41db2dc8715e9964f6d79e158c154ba
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-08 02:02:08 -0300

    chore:  deposit token service enabled

--- app/services/wallet/types.ts
+++ app/services/wallet/types.ts
@@ -1,3 +1,11 @@
+import type { RequestBuilderInitArgs } from '~/utils/request-builder';
+
+export type DepositTokenArgs = Pick<
+    RequestBuilderInitArgs,
+    'provider' | 'signerAddress'
+> & {
+    amount: bigint;
+};
 export type GetEtherBalanceResponse = {
     balance: string;
 };
