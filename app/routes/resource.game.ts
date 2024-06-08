89878de366e83a16c8375ec968c3a654385e5ef4
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-08 02:17:35 -0300

    chore(routes): routes use case types consume adjusted

--- app/routes/resource.game.ts
+++ app/routes/resource.game.ts
@@ -6,13 +6,21 @@ import { HttpStatus } from '~/utils/http';
 
 import type { ActionFunctionArgs } from '@remix-run/node';
 import type { ClientActionFunctionArgs } from '@remix-run/react';
-import type { PreActionResponse } from '~/use-cases/governance/type';
+import type { GovernanceFormData } from '~/use-cases/governance/type';
+import type { GetWalletResponse } from '~/use-cases/wallet/type';
+
+type ServerActionResponse = {
+    wallet?: GetWalletResponse;
+    formData?: GovernanceFormData;
+    status?: number;
+};
 
 export async function action({ request }: ActionFunctionArgs) {
     if (new URL(request.url).searchParams.has('reset'))
         return json({ status: HttpStatus.ResetContent }); //HTTP RESET STATUS
 
-    const preActionRes = await governanceUseCase.preAction(request);
+    const preActionRes = await governanceUseCase.governancePreAction(request);
+
     if (preActionRes.error)
         throw new Response(preActionRes.error, {
             status: preActionRes.status
@@ -22,44 +30,52 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export async function clientAction({ serverAction }: ClientActionFunctionArgs) {
-    const serverRes = await serverAction<Required<PreActionResponse>>();
+    const serverRes = await serverAction<ServerActionResponse>();
 
     if (serverRes.status === HttpStatus.ResetContent) return {};
 
     const { formData, wallet } = serverRes;
-    const {
-        functionName,
-        functionCode,
-        gameId,
-        validatorFunctionName,
-        ...leftovers
-    } = formData;
 
-    if (wallet.isDAOMember && functionName && functionCode) {
-        return await governanceUseCaseClient.handleSubmitCreateValidationFunction(
-            {
-                provider: window.ethereum,
-                signerAddress: wallet.address!,
-                functionName,
-                functionCode
-            }
-        );
-    } else if (wallet.isDAOMember && validatorFunctionName) {
-        return {
-            ...(await governanceUseCaseClient.handleSubmitCreateGame({
-                provider: window.ethereum,
-                signerAddress: wallet.address!,
-                validatorFunctionName,
-                ...leftovers
-            })),
-            refetch: true
-        };
-    } else if (gameId) {
-        return await gameUseCaseCliente.submitPlaceABet({
-            provider: window.ethereum,
-            signerAddress: wallet.address!,
+    if (formData && wallet) {
+        const {
+            functionName,
+            functionCode,
             gameId,
+            validatorFunctionName,
             ...leftovers
-        });
+        } = formData;
+
+        if (wallet.isDAOMember && functionName && functionCode) {
+            return await governanceUseCaseClient.handleSubmitCreateValidationFunction(
+                {
+                    provider: window.ethereum,
+                    signerAddress: wallet.address!,
+                    functionName,
+                    functionCode
+                }
+            );
+        } else if (wallet.isDAOMember && validatorFunctionName) {
+            return {
+                ...(await governanceUseCaseClient.handleSubmitCreateGame({
+                    provider: window.ethereum,
+                    signerAddress: wallet.address!,
+                    validatorFunctionName,
+                    ...leftovers
+                })),
+                refetch: true
+            };
+        } else if (gameId) {
+            return await gameUseCaseCliente.submitPlaceABet({
+                provider: window.ethereum,
+                signerAddress: wallet.address!,
+                gameId,
+                ...leftovers
+            });
+        }
     }
+
+    return {
+        ok: false,
+        error: 'Neither wallet nor formData were provided.'
+    };
 }
