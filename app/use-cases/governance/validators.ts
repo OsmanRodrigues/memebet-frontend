bcce1bdf969be73481a57bbeeafebc854bb9b3ed
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-09 20:02:56 -0300

    enhanc: submit game validator impl

--- app/use-cases/governance/validators.ts
+++ app/use-cases/governance/validators.ts
@@ -1,26 +1,31 @@
 import { validateSchema } from '~/utils/validator';
-import { setLocale, string } from 'yup';
+import { number, setLocale, string } from 'yup';
 
 import type { ValidationError } from 'yup';
-import { CreateValidationFunctionArgs } from '~/services/governance/type';
+import type {
+    CreateValidationFunctionArgs,
+    CreateGameArgs
+} from '~/services/governance/type';
 
-setLocale({
-    mixed: {
-        default: 'data is invalid'
-    },
-    string: {
-        min: '${path} must be ${min} in length',
-        matches:
-            '${path} be a whitespaced arrow function (eg.: "const myfn = (...) => ...") or a regular function statement'
-    }
-});
+const functionNameSize = 4;
 
 export const validateSubmitCreateValidationFunctionData = async (
     data: Pick<CreateValidationFunctionArgs, 'functionCode' | 'functionName'>
-): Promise<{ error?: any; message?: string } | undefined> =>
-    validateSchema(
+): Promise<{ error?: any; message?: string } | undefined> => {
+    setLocale({
+        mixed: {
+            default: 'data is invalid'
+        },
+        string: {
+            min: '${path} must be ${min} in length',
+            matches:
+                '${path} be a whitespaced arrow function (eg.: "const myfn = (...) => ...") or a regular function statement'
+        }
+    });
+
+    return validateSchema(
         {
-            functionName: string().min(4).required(),
+            functionName: string().min(functionNameSize).required(),
             functionCode: string()
                 .min(15)
                 .matches(
@@ -35,3 +40,43 @@ export const validateSubmitCreateValidationFunctionData = async (
             error: err.name,
             message: `Invalid create function data: ${err.errors[0]}`
         }));
+};
+export const validateSubmitCreateGameData = async (
+    data: Pick<
+        CreateGameArgs,
+        'away' | 'end' | 'home' | 'start' | 'token' | 'validatorFunctionName'
+    >
+): Promise<{ error?: any; message?: string } | undefined> => {
+    setLocale({
+        mixed: {
+            default: 'data is invalid'
+        },
+        string: {
+            min: '${path} must be ${min} in length',
+            matches: '${path} must be a 0x(...) token address'
+        },
+        number: {
+            min: ({ path }) =>
+                path === 'end'
+                    ? `the ${path} must be a future date and time than the start.`
+                    : `the ${path} must be a future date and time than now.`
+        }
+    });
+
+    return validateSchema(
+        {
+            away: string().min(3).required(),
+            end: number().integer().min(data.start).required(),
+            home: string().min(3).required(),
+            start: number().integer().min(Date.now()).required(),
+            token: string().min(3).matches(/^0x/).required(),
+            validatorFunctionName: string().min(functionNameSize).required()
+        },
+        data
+    )
+        .then(() => undefined)
+        .catch((err: ValidationError) => ({
+            error: err.name,
+            message: `Invalid create game data: ${err.errors[0]}`
+        }));
+};
