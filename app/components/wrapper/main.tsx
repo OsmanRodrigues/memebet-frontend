5277c92a542c28e6dd1de56a4fd15135daea3376
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-10 01:41:41 -0300

    chore: main wrapper mobile pd set

--- app/components/wrapper/main.tsx
+++ app/components/wrapper/main.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react';
 
 export const MainWrapper = ({ children }: PropsWithChildren) => (
-    <main className="min-h-screen px-8">{children}</main>
+    <main className="min-h-screen px-14 mobile:px-8">{children}</main>
 );
