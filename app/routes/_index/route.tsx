ec9e70556846c5459fbc5adecd163e5cdda0d45d
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-10 04:03:37 -0300

    feat: how it works section impl

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,9 +1,13 @@
+import { Spacer } from '@nextui-org/react';
 import { HeroSection } from './hero.section';
+import { HowItWorksSection } from './how-it-works.section';
 
 export default function Home() {
     return (
         <>
             <HeroSection />
+            <Spacer y={12} />
+            <HowItWorksSection />
         </>
     );
 }
