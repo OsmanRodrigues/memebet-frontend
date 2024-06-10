39589e635a207b78505fc94a737748be3c35e8e2
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-10 00:51:35 -0300

    feat: hero section animation impl

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,18 +1,9 @@
-import { useFetcher } from '@remix-run/react';
-import type { WalletData } from '~/use-cases/wallet/type';
+import { HeroSection } from './hero.section';
 
 export default function Home() {
-    const fetcher = useFetcher<WalletData>({ key: 'auth-fetcher' });
-
     return (
         <>
-            <h1>
-                Hello world! Private key:
-                {fetcher.data?.address ?? 'not provided'}
-            </h1>
-            <h1>
-                Current balance: {fetcher.data?.ethBalance ?? 'not informed'}
-            </h1>
+            <HeroSection />
         </>
     );
 }
